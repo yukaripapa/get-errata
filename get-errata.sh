fdca6b7e9ff4f44c78fb5f9634cb4d560a7a2592
Tsuyoshi Nagata <yukari.papa@gmail.com>
2023-04-17 14:08:24 +0900

    for AUS downloading

--- get-errata.sh
+++ get-errata.sh
@@ -31,7 +31,7 @@
 file_name=$1
 # 拡張子を変更しダウンロードスクリプト名とする
 sh_filename="${file_name%.*}.sh"
-egrep -m 1 -A 400 "^.h2.Red Hat Enterprise Linux.*x86_64" $file_name | grep auth_= |gawk '{print $5}'|sort -u|grep "src.rpm\|x86_64\|noarch"|gawk -F">" '{print $1}'|sed 's/href=//g'|sed 's/\&amp;/\&/g'|gawk -F"[/?]" '{print "curl --output " $11 " " $0}' > $sh_filename
+egrep -m 1 -A 400 "^.h2.Red Hat Enterprise Linux.*(x86_64|AUS)" $file_name | grep auth_= |gawk '{print $5}'|sort -u|grep "src.rpm\|x86_64\|noarch"|gawk -F">" '{print $1}'|sed 's/href=//g'|sed 's/\&amp;/\&/g'|gawk -F"[/?]" '{print "curl --output " $11 " " $0}' > $sh_filename
 #  何故 400行としたか？ 2023yの時点でx86_64カーネルエラッタの取得が htmlテーブル上に 260行程度で記載されており、
 # 将来パッケージ数の増加に対応できるようにした。(product B欄は IBMzなので２番目のgrepで除外される。)
 #
