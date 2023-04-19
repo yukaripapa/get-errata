ad3dc0c54230ff9c3b43bfba3ef1f556a3cdbef2
yukaripapa <yukari.papa@gmail.com>
2023-04-19 12:05:29 +0900

    Add ELS patterns

--- get-errata.sh
+++ get-errata.sh
@@ -32,18 +32,18 @@ VERSION="2.0.2"
 file_name=$1
 # 拡張子を変更しダウンロードスクリプト名とする
 sh_filename="${file_name%.*}.sh"
-egrep -m 1 -A 400 "^.h2.Red Hat Enterprise Linux.*(x86_64|AUS)" $file_name | grep auth_= |gawk '{print $5}'|sort -u|grep "src.rpm\|x86_64\|noarch"|gawk -F">" '{print $1}'|sed 's/href=//g'|sed 's/\&amp;/\&/g'|gawk -F"[/?]" '{print "curl --output " $11 " " $0}' > $sh_filename
+egrep -m 1 -A 400 "^.h2.Red Hat Enterprise Linux.*(x86_64|AUS|(Extended Life Cycle Support))" $file_name | grep auth_= |gawk '{print $5}'|sort -u|grep "src.rpm\|x86_64\|noarch"|gawk -F">" '{print $1}'|sed 's/href=//g'|sed 's/\&amp;/\&/g'|gawk -F"[/?]" '{print "curl --output " $11 " " $0}' > $sh_filename
 #  何故 400行としたか？ 2023yの時点でx86_64カーネルエラッタの取得が htmlテーブル上に 260行程度で記載されており、
 # 将来パッケージ数の増加に対応できるようにした。(product B欄は IBMzなので２番目のgrepで除外される。)
 #
 # ダウンロードスクリプトの実行
-#sh $sh_filename
+sh $sh_filename
 sha256sum *rpm >sha256sum.txt
 # ダウンロードしたrpmの数を数えておく27以下だと失敗しているかも。
 wc sha256sum.txt
 gawk '{print $1}' sha256sum.txt >sha256key
 # ダウンロード元のチェックサムを集める。
-egrep -m 1 -A 400 "^.h2.Red Hat Enterprise Linux.*x86_64" $file_name | grep -B2 "src.rpm\|x86_64\|noarch" |grep SHA-256:|sort -u|gawk -F"<" '{print $2}'|gawk '{print $3}' >sha256sum.lst  
+egrep -m 1 -A 400 "^.h2.Red Hat Enterprise Linux.*(x86_64|AUS|(Extended Life Cycle Support))" $file_name | grep -B2 "src.rpm\|x86_64\|noarch" |grep SHA-256:|sort -u|gawk -F"<" '{print $2}'|gawk '{print $3}' >sha256sum.lst  
 echo 'checking original'
 grep -v -f sha256key sha256sum.lst
 result=$(grep -v -f sha256key sha256sum.lst )
